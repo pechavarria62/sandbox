bc5769eb476f287a3bc81eb0b369efcf5b908c99
Pedro Echavarria <pechavarria62@gmail.com>
2022-11-02 12:48:13 -0400

    updates 11.02.2022

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 # by Pedro Echavarria
 #***********************
 #((((((((((((((((((((((((( Imports )))))))))))))))))))))))))
-from math import degrees
+from math import degrees, factorial, log
 import random
 import turtle
 #((((((((((((((((((((((((( Imports )))))))))))))))))))))))))
@@ -58,7 +58,7 @@ print("sum =", add(num1,num2), "product =", multiply(num1,num2))
 
 #__________________________________________________________________________________________________
 
-# A set of functions that call eachother
+# A set of functions that call each other
 def f(x): 
 	x = x-1
 	return g(x)+1
@@ -473,7 +473,7 @@ def count_fricatives(list):
             count += i.upper().count(consonants)
     return count
 
-list = ['Zebra', 'lightsaber', '1234 JOYS!']
+list = ['Zebra', 'light saber', '1234 JOYS!']
 print(count_fricatives(list))
 print(count_fricatives(list) == 3)
 #__________________________________________________________________________________________________
@@ -495,12 +495,30 @@ while True:
     num = input("enter a number (“Q” to quit)? ")
     if num != name :
         contar += num # type: ignore
-        total = contar.count("0") # type: ignore
+        result = contar.count("0") # type: ignore
     else:
-        print("You entered", total, "Zeros")# type: ignore
+        print("You entered", result + " Zeros") # type: ignore
         break
 
 #__________________________________________________________________________________________________
+
+# Program that asks the user for their favorite month and day of the month as input and outputs
+# The month & day followed by the year, Whether or not the month is alphanumeric, The number of
+# 2’s in the number The factorial of the number and The log base 2 of the absolute value of the number.
+
+def day():
+    favorite_month = input("Please enter your favorite month: ")
+    favorite_day = input("Enter your favorite day of the month: ")
+    str1 = favorite_month[0].upper()
+    str2 = favorite_month[1:].lower()
+    print(str1 + str2, favorite_day , ", 2020")
+    print("Is the month alphanumeric?", favorite_month .isalnum())
+    print("How many 2’s?",favorite_day.count("2"))
+    favorite_day = int(favorite_day)
+    print("Factorial: ", factorial(favorite_day)) 
+    print("Log base 2: ", log(abs(favorite_day), 2)) 
+
+day()
 #__________________________________________________________________________________________________
 #__________________________________________________________________________________________________
 #__________________________________________________________________________________________________
